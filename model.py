1f11fbc130a7ce405401d3027c194624a997235a
Felix Coy <koyle@Felixs-MacBook-Pro.local>
2025-04-13 23:16:08 +0200

    added basic rl classes

--- model.py
+++ model.py
@@ -1,11 +1,21 @@
 import gymnasium as gym
 
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.utils import set_random_seed
+import torch
+from modelCallback import modelCallback 
 
+class model(PPO):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+    def load_weights(self, source: str = "ppo_weights.pth"):
+        #weights has to point to a file.pth (source)
+        self.policy.load_state_dict(torch.load(source))
 
+    def model_train(self, save_freq: int =1000, save_path: str ="ppo_weights.pth", timesteps: int = 1000000000000, threshhold : int = 0.01):
+        callback = modelCallback(save_freq, save_path, threshhold)
+        self.learn(timesteps, callback=callback)
 
+    def model_evaluate():
+        pass
 
