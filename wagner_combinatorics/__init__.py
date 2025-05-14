8db5f91fc9eaab0539072423fcc87a5e141e7b62
Frederik Dammeier <code@fdammeier.de>
2025-05-14 20:58:48 +0200

    added code of wagner, some refactoring to make running easier

---
+++ wagner_combinatorics/__init__.py
@@ -0,0 +1,5 @@
+"""This module contains the original logic used by A Z Wagner in https://arxiv.org/abs/2104.14516.
+
+The code is largely copied from their original github repository at https://github.com/zawagner22/cross-entropy-for-combinatorics
+with some refactoring having been made to improve performance and evaluation capabilities.
+"""
