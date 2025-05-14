24c5c84c18d3026f02c26af830fddbbf517ceffa
ci72buri <ci72buri@clara04.sc.uni-leipzig.de>
2025-05-14 23:13:00 +0200

    added A2C, DQN and DDQN to model.py; Updated conda_env.yaml

--- model.py
+++ model.py
@@ -1,21 +1,34 @@
 import gymnasium as gym
-
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN, A2C
 import torch
-from modelCallback import modelCallback 
+from modelCallback import modelCallback
 
-class model(PPO):
+class BaseModel:
+    def load_weights(self, source: str):
+        self.policy.load_state_dict(torch.load(source))
+
+    def model_train(self, save_freq=1000, save_path="model.pth", timesteps=1_000_000, threshhold=0.01):
+        callback = modelCallback(save_freq, save_path, threshhold)
+        self.learn(total_timesteps=timesteps, callback=callback)
+
+
+class Model_PPO(PPO, BaseModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def load_weights(self, source: str = "ppo_weights.pth"):
-        #weights has to point to a file.pth (source)
-        self.policy.load_state_dict(torch.load(source))
 
-    def model_train(self, save_freq: int =1000, save_path: str ="ppo_weights.pth", timesteps: int = 1000000000000, threshhold : int = 0.01):
-        callback = modelCallback(save_freq, save_path, threshhold)
-        self.learn(timesteps, callback=callback)
+class Model_DQN(DQN, BaseModel):
+    def __init__(self, *args, **kwargs):
+        # DQN (no DDQN): optimize_memory_usage=False
+        super().__init__(*args, optimize_memory_usage=False, **kwargs)
 
-    def model_evaluate():
-        pass
 
+class Model_DDQN(DQN, BaseModel):
+    def __init__(self, *args, **kwargs):
+        # Double DQN: optimize_memory_usage=True
+        super().__init__(*args, optimize_memory_usage=True, **kwargs)
+
+
+class Model_A2C(A2C, BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
