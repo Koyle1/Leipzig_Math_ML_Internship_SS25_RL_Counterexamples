cd68b1bc66f60be6edf4153999d4719f168962d5
ci72buri <ci72buri@clara02.sc.uni-leipzig.de>
2025-05-15 18:22:11 +0200

    Added additional information about the toal number of episodes

--- combinatorics_25/callbacks/modelCallback.py
+++ combinatorics_25/callbacks/modelCallback.py
@@ -7,16 +7,22 @@ class modelCallback(BaseCallback):
         self.save_freq = save_freq
         self.save_path = save_path
         self.threshhold = threshhold
+        self.episode_count = 0
 
     def _on_step(self) -> bool:
         #To be updated for interrupting when counterexample is found
+        infos = self.locals.get("infos", [])
+        for info in infos:
+            if "episode" in info:
+                self.episode_count += 1
+        
         if self.n_calls % self.save_freq == 0:
             torch.save(self.model.policy.state_dict(), self.save_path)
-            print(f"Reward: {self.locals.get('rewards')}")
-            print(f"Dones: {self.locals.get('dones')}")
-            print(f"Info: {self.locals.get('infos')}")
             print(f"Saved weights at step {self.n_calls}")
             value_loss = self.model.logger.name_to_value.get("train/value_loss")
             #Change for non sequential enviorment
             
         return True
+
+    def _on_training_end(self) -> None:
+        print(f"\n[Callback] Total episodes completed: {self.episode_count}")
