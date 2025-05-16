c7f5372e6779d19c0c56bae0017a959dae1cab1d
AnnikaSo <annika.hirt@web.de>
2025-05-16 12:05:51 +0200

    Add folders, delete duplicate files, add to gitignore

--- combinatorics_25/train.py
+++ combinatorics_25/train.py
@@ -1,8 +1,8 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
-from model import Model
+from model.model import Model
 import gymnasium as gym
-import enviroment.registration
+import environment.registration
 
 import argparse
 
