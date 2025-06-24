bf676a4ec81314ecd4b554084976ac4c6a963f7b
Frederik Dammeier <code@fdammeier.de>
2025-06-24 14:00:45 +0200

    slight simplification

--- combinatorics_25/environment/graphNodeBuildPEseqBoundaryNewRewardOnlyOnNewEdge.py
+++ combinatorics_25/environment/graphNodeBuildPEseqBoundaryNewRewardOnlyOnNewEdge.py
@@ -135,6 +135,7 @@ class GraphNodeBuildEnv(gym.Env):
         self.obs = np.concatenate([adj_flat, node_scalar, edge_scalar])  # Final shape: (363,)
 
     def _recalculate_reward(self):
+        # Includes surrogate model
         if self.use_surrogate and self.surrogate_model is not None:
             with torch.no_grad():
                 obs_tensor = torch.tensor(self.obs, dtype=torch.float32).unsqueeze(0).to(self.device)
@@ -144,6 +145,7 @@ class GraphNodeBuildEnv(gym.Env):
 
     def step(self, action):
         assert self.action_space.contains(action)
+        reward = self.previous_reward # return the previous reward by default
 
         # Add edge (and next node) to graph
         if self.current_node < N:
@@ -153,18 +155,16 @@ class GraphNodeBuildEnv(gym.Env):
                 self.graph.add_edge(self.current_node, target_node)
 
                 # Recalculate reward if a new edge has been added   
-                reward = self._recalculate_reward()
-                self.previous_reward = reward
-            else: # Otherwise return the previous reward
-                reward = self.previous_reward
+                reward = calc_reward_nx(self.graph, fiedler_score = self.min_fiedler)
+                self.previous_reward = reward                
 
             self.current_edge_idx += 1
 
             if self.current_edge_idx == self.current_node:
                 # Always recalculate the reward at the end of a node to discourage fully disconnected nodes
                 # The reward is already calculated for the graph if action == 1
-                if action != 1:
-                    reward = self._recalculate_reward()
+                if not action == 1:
+                    reward = calc_reward_nx(self.graph, fiedler_score = self.min_fiedler)
                     self.previous_reward = reward
                 
                 self.graph.add_node(self.current_node)
